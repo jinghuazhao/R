c4ff4da190454b6d7280bb2da03ccd2cf3f8ca03
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-26 14:36:19 +0000

    linear mixed model

---
+++ lmm/src/package_native_routine_registration_skeleton.c
@@ -0,0 +1,48 @@
+#include <R_ext/RS.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/*
+  The following name(s) appear with different usages
+  e.g., with different numbers of arguments:
+
+    fastmode
+
+  This needs to be resolved in the tables and any declarations.
+*/
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Fortran calls */
+extern void F77_NAME(cebayes)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(ecmeml)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(ecmerml)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(fastmcmc)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(fastml)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(fastmode)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(fastmode)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(fastrml)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(mgibbs)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(rngs)(void *);
+
+static const R_FortranMethodDef FortranEntries[] = {
+    {"cebayes",  (DL_FUNC) &F77_NAME(cebayes),  24},
+    {"ecmeml",   (DL_FUNC) &F77_NAME(ecmeml),   43},
+    {"ecmerml",  (DL_FUNC) &F77_NAME(ecmerml),  46},
+    {"fastmcmc", (DL_FUNC) &F77_NAME(fastmcmc), 50},
+    {"fastml",   (DL_FUNC) &F77_NAME(fastml),   47},
+    {"fastmode", (DL_FUNC) &F77_NAME(fastmode), 50},
+    {"fastmode", (DL_FUNC) &F77_NAME(fastmode), 51},
+    {"fastrml",  (DL_FUNC) &F77_NAME(fastrml),  50},
+    {"mgibbs",   (DL_FUNC) &F77_NAME(mgibbs),   41},
+    {"rngs",     (DL_FUNC) &F77_NAME(rngs),      1},
+    {NULL, NULL, 0}
+};
+
+void R_init_lmm(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
