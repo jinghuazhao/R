fabf057b865d810846f8badd242cc8130f6bb798
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-05 15:26:30 +0100

    st.sh

---
+++ st.sh
@@ -0,0 +1,30 @@
+# 5-6-2018 JHZ
+
+git add .gitignore
+git commit -m "These are ignored"
+git add CGR_1.0-5.tar.gz
+git commit -m "Classic Genetics in R"
+git add kinship_1.1.4.tar.gz
+git commit -m "kinship package"
+git add kinship_1.1.4.zip
+git commit -m "kinship for Windows"
+git add CGR
+git commit -m "Classic Genetics in R"
+git add gap
+git commit -m "genetic analysis package"
+git add gap.datasets
+git commit -m "Datasets for 'gap'"
+git add lmm
+git commit -m "linear mixed model"
+git add pan
+git commit -m "Multiple imputation for multivariate panel or clustered dat"
+git add kinship
+git commit -m "mixed-effects Cox models, sparse matrices, and modeling data from large pedigrees"
+git add tdthap
+git commit -m "Transmission/disequilibrium tests for extended marker haplotypes"
+git add README.md
+git commit -m "README"
+git add st.sh
+git commit -m "st.sh"
+git push
+
