74c0a95cbf898848738108bb59643fbf3399f50c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-29 22:34:48 +0100

    utils

---
+++ utils/R-packages.R
@@ -0,0 +1,4 @@
+# gap
+install.packages(c("BradleyTerry2","MASS","MCMCglmm","R2jags","bdsmatrix","coda","coxme","genetics","haplo.stats","magic","mets","pedigreemm","regress","rms"))
+# pan
+install.packages("mitools")
