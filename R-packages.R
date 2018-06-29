6c23c13ad548ad11070d369ec71629d18e1c406d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-29 22:33:07 +0100

    removed

--- R-packages.R
+++
@@ -1,4 +0,0 @@
-# gap
-install.packages(c("BradleyTerry2","MASS","MCMCglmm","R2jags","bdsmatrix","coda","coxme","genetics","haplo.stats","magic","mets","pedigreemm","regress","rms"))
-# pan
-install.packages("mitools")
