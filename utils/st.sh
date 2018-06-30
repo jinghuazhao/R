1c3949b9f4a08be9f1bf87d65ddd87a4033efaa0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-30 16:28:32 +0100

    utils

--- utils/st.sh
+++ utils/st.sh
@@ -4,6 +4,8 @@ git add .gitignore
 git commit -m "These are ignored"
 git add CGR CGR_1.0-5.tar.gz
 git commit -m "Classic Genetics in R"
+git add kinship_1.1.4.tar.gz kinship_1.1.4.zip
+git commit -m "kinship source (.tar.gz) and Windows (.zip) packages"
 git add gap
 git commit -m "genetic analysis package"
 git add gap.datasets
