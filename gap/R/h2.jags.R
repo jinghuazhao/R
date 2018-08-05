7c0f85193773ebae96b2bc016689b6a9cf8d6a57
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-08-05 21:23:27 +0100

    genetic analysis package

--- gap/R/h2.jags.R
+++ gap/R/h2.jags.R
@@ -1,7 +1,8 @@
 h2.jags <- function(y,x,G,eps=0.0001,sigma.p=0,sigma.r=1,parms=c("b","p","r","h2"),...)
 {
+  M <- ncol(x)
   N <- length(y)
-  data=list(N=N,eps=eps,y=y,x=x,CG=t(chol(G+diag(eps,N))))
+  data=list(M=M,N=N,eps=eps,y=y,x=x,CG=t(chol(G+diag(eps,N))))
   inits=function() list(b=rep(0,ncol(x)),sigma.p=sigma.p,sigma.r=sigma.r)
   modelfile <- system.file(package="gap","JAGS","h2.jags")
   jagsfit <- R2jags::jags(data, inits, parms, model.file=modelfile, ...)
