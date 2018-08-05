5cef6200a301d4d22f89197598fe992223c84b98
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-08-05 20:59:49 +0100

    utils

--- utils/R-packages.R
+++ utils/R-packages.R
@@ -1,4 +1,4 @@
 # gap
-install.packages(c("BradleyTerry2","MASS","MCMCglmm","R2jags","bdsmatrix","coda","coxme","genetics","haplo.stats","magic","mets","pedigreemm","regress","rms"))
+install.packages(c("BradleyTerry2","MASS","MCMCglmm","R2jags","bdsmatrix","coda","coxme","gap.datasets","genetics","haplo.stats","kinship2","magic","mets","pedigreemm","regress","rms"))
 # pan
 install.packages("mitools")
