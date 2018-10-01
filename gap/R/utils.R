00c1855d469c5dafffb69d647dfb42d51991c0e4
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-10-01 20:10:10 +0100

    genetic analysis package

--- gap/R/utils.R
+++ gap/R/utils.R
@@ -516,8 +516,7 @@ ReadGRMPCA <- function(prefix)
   invisible(list(N=N,posGRM=posGRM,negGRM=negGRM,id=id))
 }
 
-# gc.lambda by Daniel R Barnes
-# Estimation of the genomic control inflation statistic (lambda)
+# gc.lambda and miamiplot functions hosted at CEU by Daniel R Barnes
 
 gc.lambda <- function(p) {
   p <- p[!is.na(p)]
@@ -528,4 +527,130 @@ gc.lambda <- function(p) {
 
   lambda <- median(obs)/median(exp)
   return(lambda)
+}
+
+miamiplot <-
+function (x, chr = "CHR", bp = "BP", p = "P", pr = "PR", snp = "SNP", 
+    col = c("midnightblue", "chartreuse4"), col2 = c("royalblue1", 
+        "seagreen1"), ymax = NULL, highlight = NULL, highlight.add = NULL, 
+    pch = 19, cex = 0.75, cex.lab = 1, xlab = "Chromosome", ylab = "-log10(P) [y>0]; log10(P) [y<0]", 
+    lcols = c("red", "black"), lwds = c(5, 2), ltys = c(1, 2), 
+    main = "", ...) 
+{
+    P = index = NULL
+    PR = index = NULL
+    if (!(chr %in% names(x))) 
+        stop(paste("Column", chr, "not found!"))
+    if (!(bp %in% names(x))) 
+        stop(paste("Column", bp, "not found!"))
+    if (!(p %in% names(x))) 
+        stop(paste("Column", p, "not found!"))
+    if (!(pr %in% names(x))) 
+        stop(paste("Column", pr, "not found!"))
+    if (!(snp %in% names(x))) 
+        if (!is.numeric(x[[chr]])) 
+            stop(paste(chr, "column should be numeric. Do you have 'X', 'Y', 'MT', etc? If so change to numbers and try again."))
+    if (!is.numeric(x[[bp]])) 
+        stop(paste(bp, "column should be numeric."))
+    if (!is.numeric(x[[p]])) 
+        stop(paste(p, "column should be numeric."))
+    if (!is.numeric(x[[pr]])) 
+        stop(paste(pr, "column should be numeric."))
+    d = data.frame(CHR = x[[chr]], BP = x[[bp]], P = x[[p]], 
+        PR = x[[pr]])
+    if (!is.null(x[[snp]])) 
+        d = transform(d, SNP = x[[snp]])
+    d = subset(d[order(d$CHR, d$BP), ], (P > 0 & P <= 1 & is.numeric(P) & 
+        PR > 0 & PR <= 1 & is.numeric(PR)))
+    d$logp = -log10(d$P)
+    d$logpr = log10(d$PR)
+    d$pos = NA
+    ymax = ceiling(max(d$logp) + 2)
+    ymin = floor(min(d$logpr) - 2)
+    d$index = NA
+    ind = 0
+    for (i in unique(d$CHR)) {
+        ind = ind + 1
+        d[d$CHR == i, ]$index = ind
+    }
+    nchr = length(unique(d$CHR))
+    if (nchr == 1) {
+        d$pos = d$BP
+        ticks = floor(length(d$pos))/2 + 1
+        xlabel = paste("Chromosome", unique(d$CHR), "position")
+        labs = ticks
+    }
+    else {
+        lastbase = 0
+        ticks = NULL
+        for (i in unique(d$index)) {
+            if (i == 1) {
+                d[d$index == i, ]$pos = d[d$index == i, ]$BP
+            }
+            else {
+                lastbase = lastbase + tail(subset(d, index == 
+                  i - 1)$BP, 1)
+                d[d$index == i, ]$pos = d[d$index == i, ]$BP + 
+                  lastbase
+            }
+            ticks = c(ticks, d[d$index == i, ]$pos[floor(length(d[d$index == 
+                i, ]$pos)/2) + 1])
+        }
+        xlabel = "Chromosome"
+        labs = unique(d$CHR)
+    }
+    xmax = ceiling(max(d$pos) * 1.03)
+    xmin = floor(max(d$pos) * -0.03)
+    plot(NULL, xaxt = "n", bty = "n", xaxs = "i", yaxs = "i", 
+        xlim = c(xmin, xmax), ylim = c(ymin, ymax), main = main, 
+        xlab = xlab, ylab = ylab, las = 1, pch = pch, cex.lab = cex.lab, 
+        ...)
+    if (nchr == 1) {
+        axis(1, ...)
+    }
+    else {
+        axis(1, at = ticks, labels = labs, ...)
+    }
+    col = rep(col, max(d$CHR))
+    col2 = rep(col2, max(d$CHR))
+    if (nchr == 1) {
+        with(d, points(pos, logpr, cex = cex, pch = pch, ...))
+        with(d, points(pos, logp, cex = cex, pch = pch, ...))
+    }
+    else {
+        icol = 1
+        for (i in unique(d$index)) {
+            with(d[d$index == unique(d$index)[i], ], points(pos, 
+                logpr, col = col2[icol], cex = cex, pch = pch, 
+                ...))
+            with(d[d$index == unique(d$index)[i], ], points(pos, 
+                logp, col = col[icol], cex = cex, pch = pch, 
+                ...))
+            icol = icol + 1
+        }
+    }
+    abline(h = -log10(5e-08), col = lcols[2], lwd = lwds[2], 
+        lty = ltys[2])
+    abline(h = log10(5e-08), col = lcols[2], lwd = lwds[2], lty = ltys[2])
+    abline(h = 0, col = lcols[1], lwd = lwds[1], lty = ltys[1])
+    if (!is.null(highlight)) {
+        if (any(!(highlight %in% d$SNP))) 
+            warning("You're trying to highlight SNPs that don't exist in your results.")
+        d.highlight = d[which(d$SNP %in% highlight), ]
+        with(d.highlight, points(pos, logp, col = "red3", cex = cex, 
+            pch = pch, ...))
+        with(d.highlight, points(pos, logpr, col = "red3", cex = cex, 
+            pch = pch, ...))
+    }
+    if (!is.null(highlight.add)) {
+        print("yessssssssssssssssss")
+        if (any(!(highlight.add %in% d$SNP))) 
+            warning("You're trying to highlight SNPs that don't exist in your results.")
+        d.highlight.add = d[which(d$SNP %in% highlight.add), 
+            ]
+        with(d.highlight.add, points(pos, logp, col = "darkgreen", 
+            cex = cex, pch = pch, ...))
+        with(d.highlight.add, points(pos, logpr, col = "darkgreen", 
+            cex = cex, pch = pch, ...))
+    }
 }
