eef224033ca1c2fb7e65845173b09807e6003234
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-18 18:24:07 +0000

    genetic analysis package

--- gap/R/utils.R
+++ gap/R/utils.R
@@ -744,6 +744,6 @@ cnvplot <- function(data=cnv)
       text(ifelse(x == 1, (start+CM[x])/2, (CM[x] + CM[x-1])/2), 0, pos = 1, offset = 0.5, ifelse(x=="23","X",x), cex=0.4)
   })
   segments(0,0,CM[x],0)
-  axis(2,line=1.2)
-  title(xlab="Chromosome",ylab="Frequency",line=3)
+  axis(2,line=0.2)
+  title(xlab="Chromosome",ylab="Frequency",line=2)
 }
