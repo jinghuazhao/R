fb0916e6e50ce9525e393334a63edc7b18416f54
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-20 11:53:08 +0000

    genetic analysis package

--- gap/R/utils.R
+++ gap/R/utils.R
@@ -741,7 +741,7 @@ cnvplot <- function(data=cnv)
   for (x in 1:n) with(subset(d,chr==paste(x)), {
       l <- ifelse(x==1,0,CM[x-1])
       segments(l+start,freq,l+end,freq,lwd="3",col=colors[x])
-      text(ifelse(x == 1, (start+CM[x])/2, (CM[x] + CM[x-1])/2), 0, pos = 1, offset = 0.5, ifelse(x=="23","X",x), cex=0.4)
+      text(ifelse(x == 1, CM[x]/2, (CM[x] + CM[x-1])/2), 0, pos = 1, offset = 0.5, ifelse(x=="23","X",x), cex=0.4)
   })
   segments(0,0,CM[x],0)
   axis(2,line=-0.5)
