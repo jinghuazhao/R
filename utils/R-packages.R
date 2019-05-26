ee9bbbe946cdd25d574043075c6da2869de117f3
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-26 20:55:09 +0100

    utils

--- utils/R-packages.R
+++ utils/R-packages.R
@@ -1,7 +1,7 @@
 # gap
 install.packages(c("BradleyTerry2","MASS","MCMCglmm","R2jags","bdsmatrix","circlize",
                    "coda","coxme","dplyr", "gap.datasets","genetics","haplo.stats","kinship2",
-                   "magic","meta", "mets","pedigree","pedigreemm", "plotris", "qqman", "regress","rms"),
+                   "magic","meta", "mets","pedigree","pedigreemm", "plotrix", "qqman", "regress","rms"),
                    repos="https://cran.r-project.org")
 # pan
 install.packages("mitools")
