13fd0cc9f2c664497189f1a511da8717acf4f7ac
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-26 20:58:37 +0100

    utils

--- utils/R-packages.R
+++ utils/R-packages.R
@@ -1,6 +1,6 @@
 # gap
-install.packages(c("BradleyTerry2","MASS","MCMCglmm","R2jags","bdsmatrix","circlize",
-                   "coda","coxme","dplyr", "gap.datasets","genetics","haplo.stats","kinship2",
+install.packages(c("BradleyTerry2","MASS","MCMCglmm","R2jags","bdsmatrix","circlize", "coda", "coxme",
+                   "dplyr", "forestplot", "gap.datasets","genetics","haplo.stats","kinship2",
                    "magic","meta", "mets","pedigree","pedigreemm", "plotrix", "qqman", "regress","rms"),
                    repos="https://cran.r-project.org")
 # pan
