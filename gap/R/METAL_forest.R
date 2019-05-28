5d4411846c4438f7289876652c064cb7975c4d58
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-28 09:28:06 +0100

    genetic analysis package

--- gap/R/METAL_forest.R
+++ gap/R/METAL_forest.R
@@ -1,29 +1,3 @@
-forestplot.forestplot <- function(tbl)
-{
-  tabletext <- cbind(c("Study",study,"Summary"),
-                       c("Effect",format(BETA,digits=3),format(tbl[i,"Effect"],digits=3)),
-                       c("SE",format(SE,digits=3),format(tbl[i,"StdErr"],digits=3)),
-                       c("N",N,tbl[i,"N"]))
-  print(tabletext)
-  requireNamespace("forestplot")
-  requireNamespace("rmeta")
-  forestplot::forestplot(tabletext,
-             c(NA,BETA,tbl[i,"Effect"]),
-             c(NA,BETA-1.96*SE,tbl[i,"Effect"]-1.96*tbl[i,"StdErr"]),
-             c(NA,BETA+1.96*SE,tbl[i,"Effect"]+1.96*tbl[i,"StdErr"]),
-             zero=0,
-             is.summary=c(TRUE,rep(FALSE,length(BETA)),TRUE),
-             boxsize=0.75,
-             col=rmeta::meta.colors(box="royalblue",line="darkblue", summary="royalblue"))
-  title(title)
-  rmeta::metaplot(BETA,SE,N,
-           labels=sprintf("%s (%.3f %.3f %.0f)",study,BETA,SE,N),
-           xlab="Effect distribution",ylab="",xlim=c(-1.5,1.5),
-           summn=tbl[i,"Effect"],sumse=tbl[i,"StdErr"],sumnn=tbl[i,"N"],
-           colors=rmeta::meta.colors(box="red",lines="blue", zero="green", summary="red", text="black"))
-  title(title)
-}
-
 METAL_forestplot <- function(tbl,all,rsid,pdf="INF1.fp.pdf",package="meta",...)
 {
   requireNamespace("dplyr")
@@ -66,7 +40,34 @@ METAL_forestplot <- function(tbl,all,rsid,pdf="INF1.fp.pdf",package="meta",...)
          requireNamespace("grid")
          grid::grid.text(title,0.5,0.9)
        }
-       else forestplot.forestplot(tbl)
+       else if(package=="forestplot")
+       {
+         tabletext <- cbind(c("Study",study,"Summary"),
+                              c("Effect",format(BETA,digits=3),format(tbl[i,"Effect"],digits=3)),
+                              c("SE",format(SE,digits=3),format(tbl[i,"StdErr"],digits=3)),
+                              c("N",N,tbl[i,"N"]))
+         print(tabletext)
+         requireNamespace("forestplot")
+         forestplot::forestplot(tabletext,
+                    c(NA,BETA,tbl[i,"Effect"]),
+                    c(NA,BETA-1.96*SE,tbl[i,"Effect"]-1.96*tbl[i,"StdErr"]),
+                    c(NA,BETA+1.96*SE,tbl[i,"Effect"]+1.96*tbl[i,"StdErr"]),
+                    zero=0,
+                    is.summary=c(TRUE,rep(FALSE,length(BETA)),TRUE),
+                    boxsize=0.75,
+                    col=rmeta::meta.colors(box="royalblue",line="darkblue", summary="royalblue"))
+         title(title)
+       }
+       else if(pakcage=="rmeta")
+       {
+         requireNamespace("rmeta")
+         rmeta::metaplot(BETA,SE,N,
+                  labels=sprintf("%s (%.3f %.3f %.0f)",study,BETA,SE,N),
+                  xlab="Effect distribution",ylab="",xlim=c(-1.5,1.5),
+                  summn=tbl[i,"Effect"],sumse=tbl[i,"StdErr"],sumnn=tbl[i,"N"],
+                  colors=rmeta::meta.colors(box="red",lines="blue", zero="green", summary="red", text="black"))
+         title(title)
+       }
      })
   }
   dev.off()
