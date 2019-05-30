064c741fb4d5e54f1d7e6da8203dbbc1746d209d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-30 16:01:17 +0100

    utils

--- utils/snprint.c
+++ utils/snprint.c
@@ -5,8 +5,8 @@ int main()
     char buffer[50];
     char* s = "geeksforgeeks";
 //  Counting and buffering the character using snprintf
-    int j = snprintf(buffer, 6, "%s\n", s);
+    int j = snprintf(buffer, 6, "%s.%s\n", s);
 //  Print the buffered string and character count
-    printf("%s, character count = %d\n", buffer, j);
+    printf("%s, character count = %04d, sizes = %03d/%02d\n", buffer, j, sizeof(buffer), sizeof(j));
     return 0;
 }
