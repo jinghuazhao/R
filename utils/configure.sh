4f2395268b5338219aa99767f9cb89a34ee0468e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-30 16:47:07 +0100

    utils

--- utils/configure.sh
+++ utils/configure.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 ./configure --enable-R-shlib \
- CC=/usr/bin/gcc \
- CXX=/usr/g++ \
- FC=/usr/bin/gfortran \
+ CC=\"/usr/bin/gcc\" \
+ CXX=\"/usr/g++\" \
+ FC=\"/usr/bin/gfortran\" \
  CFLAGS="-g -O2 -Wall -pedantic -mtune=native" \
  FFLAGS="-g -O2 -mtune=native -Wall -pedantic" \
  CXXFLAGS="-g -O2 -Wall -pedantic -mtune=native -Wno-ignored-attributes -Wno-deprecated-declarations -Wno-parentheses" \
