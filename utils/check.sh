ffc5b2d5ff343337cfbe7b0d3bed330dc9cacd37
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-06 16:37:39 +0100

    utils

--- utils/check.sh
+++ utils/check.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
-R-devel CMD check --install-args=" \
- CC=\"/usr/bin/gcc\" \
- CXX=\"/usr/g++\" \
- FC=\"/usr/bin/gfortran\" \
- CFLAGS=\"-g -O2 -Wall -pedantic -mtune=native\" \
- FFLAGS=\"-g -O2 -mtune=native -Wall -pedantic\" \
- CXXFLAGS=\"-g -O2 -Wall -pedantic -mtune=native -Wno-ignored-attributes -Wno-deprecated-declarations -Wno-parentheses\" \
- LDFLAGS=\"-L/usr/lib64 -L/usr/lib64" $1
+R-devel CMD check --as-cran $1
+
+export CC=/usr/bin/gcc
+export CXX=/usr/g++
+export FC=/usr/bin/gfortran
+export CFLAGS="-g -O2 -Wall -pedantic -mtune=native"
+export FFLAGS="-g -O2 -mtune=native -Wall -pedantic"
+export CXXFLAGS="-g -O2 -Wall -pedantic -mtune=native -Wno-ignored-attributes -Wno-deprecated-declarations -Wno-parentheses"
+export LDFLAGS="-L/usr/lib64 -L/usr/lib64"
+
+R-devel CMD INSTALL $1
