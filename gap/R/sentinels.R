77ce2849876b9b8e1ce4c05d62960c8bd776788e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-06 15:19:10 +0100

    genetic analysis package

--- gap/R/sentinels.R
+++ gap/R/sentinels.R
@@ -1,7 +1,7 @@
 sentinels <- function(p,st,debug=FALSE,flanking=1e+6)
 {
   Effect <- End <- StdErr <- NA
-  prot <- unique(p["prot"])
+  prot <- as.character(unique(p["prot"]))
   nr <- nrow(p)
   z <- within(p[st:nr,],{
     d <- c(0,diff(End))
