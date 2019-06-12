6b5f71179a3d6d1115d0492acf0996b9b1bafd12
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-12 16:07:40 +0100

    genetic analysis package

--- gap/R/mhtplot.trunc.R
+++ gap/R/mhtplot.trunc.R
@@ -12,7 +12,7 @@ mhtplot.trunc <- function (x, chr = "CHR", bp = "BP", p = "P", snp = "SNP", z = 
      }
   }
   if (y.brk2 <= y.brk1) stop("y.brk2 must be larger than y.brk1")
-  CHR <- BP <- P <- index <- NULL
+  CHR <- BP <- P <- index <- Z <- NULL
   if (!(chr %in% names(x))) stop(paste("Column", chr, "not found!"))
   if (!(bp %in% names(x)))  stop(paste("Column", bp, "not found!"))
   if (!(p %in% names(x)))   stop(paste("Column", p, "not found!"))
