105b71a8599f1c390e1c252c247f8a307bb7b416
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-12 15:18:55 +0100

    genetic analysis package

--- gap/R/sentinels.R
+++ gap/R/sentinels.R
@@ -1,32 +1,34 @@
-sentinels <- function(p,pid,st,debug=FALSE,flanking=1e+6)
+sentinels <- function(p,pid,st,debug=FALSE,flanking=1e+6,chr="Chrom",pos="End",b="Effect",se="StdErr",snp="MarkerName")
 {
-  Effect <- End <- StdErr <- NA
+  Effect <- p[[b]]
+  End <- p[[pos]]
+  StdErr <- p[[se]]
   nr <- nrow(p)
   z <- within(p[st:nr,],{
     d <- c(0,diff(End))
     s <- cumsum(d)
     log10p <- -log10p(Effect/StdErr)
   })
-  if (debug) print(z[c("Chrom","End","d","s","MarkerName","P.value")])
+  if (debug) print(z[c(chr,pos,"d","s",snp,"log10p")])
   if (tail(z[,"s"], 1) <= flanking) {
-    l <- head(z[,"End"], 1)
-    u <- tail(z[,"End"], 1)
+    l <- head(z[[pos]], 1)
+    u <- tail(z[[pos]], 1)
     log10p1 <- with(z, max(log10p))
     x <- subset(z, log10p==log10p1)
     r1 <- row.names(x)[1]
-    m <- tail(x[,"End"], 1)
-    n <- tail(x[,"MarkerName"], 1)
+    m <- tail(x[[pos]], 1)
+    n <- tail(x[[snp]], 1)
     cat(pid, n, l, u, u-l, log10p1, r1, "I\n", sep=",")
   } else {
     s <- subset(z, s <= flanking)
-    l <- head(s[,"End"], 1)
-    u <- tail(s[,"End"], 1)
+    l <- head(s[[pos]], 1)
+    u <- tail(s[[pos]], 1)
     log10p1 <- with(s, max(log10p))
     x <- subset(s, log10p==log10p1)
     r1 <- tail(row.names(x), 1)
-    m <- tail(x[,"End"], 1)
-    n <- tail(x[,"MarkerName"], 1)
-    t <- subset(z, End > m & End <= m + flanking)
+    m <- tail(x[[pos]], 1)
+    n <- tail(x[[snp]], 1)
+    t <- subset(z, z[[pos]] > m & z[[pos]] <= m + flanking)
     if (nrow(t)==0) {
       cat(pid, n, l, u, u-l, log10p1, r1, "II\n", sep=",")
       message(paste0("No variants +1 MB downstream so move to next block (",pid,")"))
@@ -35,7 +37,7 @@ sentinels <- function(p,pid,st,debug=FALSE,flanking=1e+6)
     } else {
       log10p2 <- with(t, max(log10p))
       y <- subset(t, log10p==log10p2)
-      u <- tail(t[,"End"], 1)
+      u <- tail(t[[pos]], 1)
       r2 <- as.numeric(tail(row.names(t), 1))
       if (log10p1 > log10p2) {
         cat(pid, n, l, u, u-l, log10p1, r1, "III\n", sep=",")
