f3bd7d421427614ebb28b311f22e613f9b4cda2b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-10-07 14:12:52 +0100

    genetic analysis package

--- gap/R/utils.R
+++ gap/R/utils.R
@@ -883,3 +883,53 @@ cs <- function(tbl, b="Effect", se="StdErr", log_p=NULL, cutoff=0.95)
   tbl <- within(tbl[ord,], {cppa <- cumsum(ppa)})
   tbl[which(with(tbl,cppa) >= cutoff)[1],]
 }
+
+xy <- function(x) if (x<23) x else if (x==23) "X" else if (x==24) "Y"
+
+hg38 <- c(248956422,242193529,198295559,190214555,181538259,170805979,159345973,145138636,138394717,133797422,135086622,133275309,
+          114364328,107043718,101991189,90338345,83257441,80373285,58617616,64444167,46709983,50818468,156040895,57227415)
+hg19 <- c(249250621,243199373,198022430,191154276,180915260,171115067,159138663,146364022,141213431,135534747,135006516,133851895,
+          115169878,107349540,102531392,90354753,81195210,78077248,59128983,63025520,48129895,51304566,155270560,59373566)
+hg18 <- c(247249719,242951149,199501827,191273063,180857866,170899992,158821424,146274826,140273252,135374737,134452384,132349534,
+          114142980,106368585,100338915,88827254,78774742,76117153,63811651,62435964,46944323,49691432,154913754,57772954)
+
+grid2d <- function(chrlen=hg19)
+{
+  CM <- cumsum(chrlen)
+  n <- length(chrlen)
+  par(xaxt = "n", yaxt = "n")
+  xy <- xy.coords(c(0,CM), c(0,CM))
+  plot(xy$x, xy$y, type = "n", ann = FALSE, axes = FALSE)
+  par(xaxt = "s", yaxt = "s", xpd = TRUE)
+  for (x in 1:n) {
+      l <- ifelse(x==1,0,CM[x-1])
+      segments(CM[x],0,CM[x],CM[n],col="black")
+      segments(0,CM[x],CM[n],CM[x],col="black")
+      text(ifelse(x == 1, CM[x]/2, (CM[x] + CM[x-1])/2), 0, pos = 1, offset = 0.5, xy(x), cex=0.8)
+      text(0, ifelse(x == 1, CM[x]/2, (CM[x] + CM[x-1])/2), pos = 2, offset = 0.5, xy(x), cex=0.8)
+  }
+  segments(0,0,CM[x],0)
+  segments(0,0,0,CM[x])
+  title(xlab="pQTL position",ylab="protein position",line=2)
+  return(list(n=n, CM=c(0,CM)))
+}
+
+mhtplot2d <- function(data)
+{
+  r <- grid2d()
+  n <- with(r, n)
+  CM <- with(r, CM)
+  d <- data[c("Chr","bp","p.chr","p.start","p.end","cis")]
+  chr1 <- d[["Chr"]]
+  chr1[chr1=="X"] <- 23
+  chr1[chr1=="Y"] <- 24
+  head(chr1)
+  pos1 <- CM[chr1] + d[["bp"]]
+  chr2 <- d[["p.chr"]]
+  chr2[chr2=="X"] <- 23
+  chr2[chr2=="Y"] <- 24
+  head(chr2)
+  pos2 <- CM[chr2] + (d[["p.start"]] + d[["p.end"]])/2
+  points(pos1,pos2,cex=0.8,col=ifelse(d[["cis"]],"red","blue"),pch=19)
+  legend("bottomright",legend=c("ciscis","trans"),box.lty=0,cex=0.8,col=c("red","blue"),horiz=TRUE,inset=c(0,1),xpd=TRUE,pch=19)
+}
