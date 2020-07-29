24b8d184baebf4cfc7d7dbf84c883678906a1ce3
jinghuazhao <jinghuazhao@hotmail.com>
2020-07-29 20:35:33 +0100

    pQTL tools

--- pQTLtools/inst/tests/st.sh
+++ pQTLtools/inst/tests/st.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+module load gcc/6
+module load pcre/8.38
+module load texlive
+
 R CMD build --resave-data pQTLtools
 R CMD check --as-cran pQTLtools_0.1.tar.gz
 R CMD INSTALL pQTLtools_0.1.tar.gz
