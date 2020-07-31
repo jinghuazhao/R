1f4ed31b21f337639d2796450085cd96958f28f1
jinghuazhao <jinghuazhao@hotmail.com>
2020-07-31 21:01:57 +0100

    pQTL tools

--- pQTLtools/NEWS.md
+++ pQTLtools/NEWS.md
@@ -1,4 +1,5 @@
 ## Version 0.1
 
+2020-07-31	Add hg19.rda,hg19Tables.rda,snpquery() but drop hgTables
 2020-07-29	Add biomaRt.rda
 2020-07-23	Import from lmm as template, save(,compress='xz')
---
+++ pQTLtools/R/snpquery.R
@@ -0,0 +1,25 @@
+snpquery <- function(query,catalogue="pQTL",proxies="EUR",p=5e-8,r2=0.7,build=37)
+{
+  ref_a1 <- ref_a2 <- ref_hg19_coordinates <- NULL
+  batches <- split(query, ceiling(seq_along(query)/100))
+  s <- t <- list()
+  for(i in 1:length(batches))
+  {
+    cat("Block ",i,"\n")
+    q <- phenoscanner::phenoscanner(snpquery=batches[[i]], catalogue=catalogue, proxies=proxies, pvalue=p, r2=r2, build=build)
+    s[[i]] <- with(q,snps)
+    t[[i]] <- with(q,results)
+  }
+  snps <- do.call(rbind,s)
+  results <- do.call(rbind,t)
+  results <- within(results,
+  {
+     a1 <- ref_a1
+     a2 <- ref_a2
+     swap <- ref_a1 > ref_a2
+     a1[swap] <- ref_a2[swap]
+     a2[swap] <- ref_a1[swap]
+     ref_snpid <- paste0(ref_hg19_coordinates,"_",a1,"_",a2)
+  })
+  list(snps=snps,results=results)
+}
