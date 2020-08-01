2ef4bd0a4243af279565f74836fe6d82f7aa18fb
jinghuazhao <jinghuazhao@hotmail.com>
2020-08-01 18:10:34 +0100

    pQTL tools

--- pQTLtools/R/snpquery.R
+++ pQTLtools/R/snpquery.R
@@ -1,21 +1,21 @@
 snpquery <- function(snplist,catalogue="pQTL",proxies="EUR",p=5e-8,r2=0.7,build=37)
 {
   ref_a1 <- ref_a2 <- ref_hg19_coordinates <- NULL
-  s <- t <- list()
   batches <- split(snplist,ceiling(seq_along(snplist)/100))
+  s <- r <- vector('list',length(batches))
   for(i in 1:length(batches))
   {
-    cat("Block ",i,"\n")
-    q <- phenoscanner::phenoscanner(snpquery=batches[[i]], catalogue=catalogue, proxies=proxies, pvalue=p, r2=r2, build=build)
+    cat("Block",i,"\n")
+    q <- phenoscanner::phenoscanner(snpquery=batches[[i]], catalogue=catalogue,
+                                    proxies=proxies, pvalue=p, r2=r2, build=build)
     s[[i]] <- with(q,snps)
-    t[[i]] <- with(q,results)
+    r[[i]] <- with(q,results)
   }
-  snps <- do.call(rbind,s)
-  results <- do.call(rbind,t)
-  results <- within(results,
+  snps <- do.call("rbind",s)
+  results <- within(do.call("rbind",r),
   {
-     a1 <- ref_a1
-     a2 <- ref_a2
+     a1 <- as.character(ref_a1)
+     a2 <- as.character(ref_a2)
      swap <- ref_a1 > ref_a2
      a1[swap] <- ref_a2[swap]
      a2[swap] <- ref_a1[swap]
