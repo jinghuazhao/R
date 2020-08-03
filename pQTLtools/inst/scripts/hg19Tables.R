ba83f74519ed49063bc552a87115dc2d3c578746
jinghuazhao <jinghuazhao@hotmail.com>
2020-08-03 09:24:34 +0100

    pQTL tools

--- pQTLtools/inst/scripts/hg19Tables.R
+++ pQTLtools/inst/scripts/hg19Tables.R
@@ -1,5 +1,7 @@
 INF <- Sys.getenv("INF")
 tsv <- paste(INF,"docs","hg19Tables.tsv",sep="/")
-hg19Tables <- subset(read.delim(tsv,as.is=TRUE),,!grepl("hap",X.chrom)&!grepl("Un",X.chrom)&!grepl("random",X.chrom)&!grepl(";",geneName)&geneName!="")
+hg19Tables <- subset(read.delim(tsv,as.is=TRUE),!grepl("hap",X.chrom)&!grepl("Un",X.chrom)&!grepl("random",X.chrom)&!grepl(";",geneName)&geneName!="")
+vars <- setdiff(names(hg19Tables),c("Actinfilin","refSeq","refSeqProt","entrezGene","ensTrans","ensProt"))
+hg19Tables <- hg19Tables[vars]
 save(hg19Tables,file="hg19Tables.rda",compress='xz')
 
