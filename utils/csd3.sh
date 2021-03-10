f73040d4859c27b9c8313776ddc91eb7cc05532c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-03-10 21:57:13 +0000

    utils

--- utils/csd3.sh
+++ utils/csd3.sh
@@ -4,7 +4,14 @@ module load gcc/6
 module load pcre/8.38
 module load texlive
 export version=4.0.4
-wget https://cran.r-project.org/src/base/R-4/R-${version}.tar.gz
+
+IFS=\. read -a fields <<<${version}
+export major=${fields[0]}
+export minor1=${fields[1]}
+export minor2=${fields[2]}
+echo ${major}.${minor1}.${minor2}
+
+wget https://cran.r-project.org/src/base/R-${main}/R-${version}.tar.gz
 tar xvfz R-${version}.tar.gz
 cd ${version}
 export prefix=/rds-d4/user/$USER/hpc-work
