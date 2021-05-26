1d38f2c10f7cf8e7ec32771cbd0322e4a4aa2e3a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-05-26 21:18:41 +0100

    genetic analysis package

--- gap/R/utils.R
+++ gap/R/utils.R
@@ -1009,28 +1009,57 @@ grid2d <- function(chrlen=hg19, plot=TRUE, cex=0.6)
   return(list(n=n, CM=c(0,CM)))
 }
 
-mhtplot2d <- function(data, plot=TRUE, cex=0.6)
-# mhtplot2d(read.table("INF1.merge.cis.vs.trans",as.is=TRUE,header=TRUE))
+#' 2D Manhattan plot
+#'
+#' @md
+#' @param d Data to be used.
+#' @param snp_name variant name.
+#' @param snp_pos variant position.
+#' @param snp_chr variant chromosome.
+#' @param gene_chr gene chromosome.
+#' @param gene_start gene start position.
+#' @param gene_end gene end position.
+#' @param protein protein name.
+#' @param gene gene name.
+#' @param lp log10(p).
+#' @param cis cis variant when TRUE.
+#' @param plot to plot when TRUE.
+#' @param cex extension factor.
+#' @export
+#' @return positional information.
+#' @examples
+#'
+#' INF <- Sys.getenv("INF")
+#' d <- read.csv(file.path(INF,"work","INF1.merge.cis.vs.trans"),as.is=TRUE)
+#' r <- mhtplot2d(d)
+
+mhtplot2d <- function(d, snp_name="SNP", snp_chr="Chr", snp_pos="bp",
+                      gene_chr="p.chr", gene_start="p.start", gene_end="p.end",
+                      protein="p.target.short", gene="p.gene", lp="log10p",
+                      cis="cis",
+                      plot=TRUE, cex=0.6)
 {
   r <- grid2d(plot=plot)
   n <- with(r, n)
   CM <- with(r, CM)
-  d <- data[c("SNP","Chr","bp","p.chr","p.start","p.end","p.target.short","p.gene","log10p","cis")]
-  chr1 <- d[["Chr"]]
+  chr1 <- data[[snpchr]]
   chr1[chr1=="X"] <- 23
   chr1[chr1=="Y"] <- 24
-  pos1 <- CM[chr1] + d[["bp"]]
-  chr2 <- d[["p.chr"]]
+  pos1 <- CM[chr1] + d[[snp_pos]]
+  chr2 <- d[[gene_chr]]
   chr2[chr2=="X"] <- 23
   chr2[chr2=="Y"] <- 24
-  mid <- (d[["p.start"]] + d[["p.end"]])/2
+  mid <- (d[[gene_start]] + d[[gene_end]])/2
   pos2 <- CM[chr2] + mid
   if (plot) {
-     points(pos1,pos2,cex=cex,col=ifelse(d[["cis"]],"red","blue"),pch=19)
-     legend("top",legend=c("cis","trans"),box.lty=0,cex=cex,col=c("red","blue"),horiz=TRUE,inset=c(0,1),xpd=TRUE,pch=19)
+     points(pos1, pos2, cex=cex, col=ifelse(d[[cis]],"red","blue"), pch=19)
+     legend("top", legend=c("cis","trans"), box.lty=0, cex=cex, col=c("red","blue"),
+            horiz=TRUE, inset=c(0,1), xpd=TRUE, pch=19)
   }
-  return(data.frame(id=d[["SNP"]],chr1=chr1,pos1=d[["bp"]],chr2=chr2,pos2=mid,x=pos1,y=pos2,
-         target=d[["p.target.short"]],gene=d[["p.gene"]],log10p=with(d,log10p),col=ifelse(d[["cis"]],"blue","red")))
+  return(data.frame(id=d[[snp_name]], chr1=chr1, pos1=d[[snp_pos]],
+                    chr2=chr2, pos2=mid, x=pos1, y=pos2,
+                    target=d[[protein]], gene=d[[gene]], log10p=d[[lp]],
+                    col=ifelse(d[[cis]],"blue","red")))
 }
 
 #' A utility to generate SNPTEST sample file
