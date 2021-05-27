81b1d47011c953910e76354983d19674eb69518a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-05-27 15:57:03 +0100

    utils

--- utils/gap.sh
+++ utils/gap.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
+function rd()
+{
+  cd gap
+  Rscript -e "devtools::document()"
+}
+
 export version=1.2.3-2
+
 R CMD build gap
 R CMD check gap_${version}.tar.gz
 R CMD INSTALL gap_${version}.tar.gz
