a3587c6a60773a5c6e4485af48880a30622a6a85
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-05-28 12:29:09 +0100

    utils

--- utils/gap.sh
+++ utils/gap.sh
@@ -4,12 +4,13 @@ function rd()
 {
   cd gap
   Rscript -e "devtools::document()"
+  cd -
+  utils/st.sh
+  Rscript -e "devtools::install_github(\"jinghuazhao/R/gap\",build_vignettes=TRUE,force=TRUE)"
 }
 
 export version=1.2.3-2
 
 R CMD build --compact-vignettes=both --md5 --resave-data gap
-R CMD check gap_${version}.tar.gz
 R CMD INSTALL --compact-docs --data-compress=xz gap_${version}.tar.gz
-
-Rscript -e "devtools::install_github(\"jinghuazhao/R/gap\",build_vignettes=TRUE,force=TRUE)"
+R CMD check gap_${version}.tar.gz
