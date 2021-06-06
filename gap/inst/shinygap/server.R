e502da3f7cae7db35edde067a79e641d0d123a54
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-06-06 18:15:17 +0100

    genetic analysis package

--- gap/inst/shinygap/server.R
+++ gap/inst/shinygap/server.R
@@ -15,7 +15,7 @@ server <- function(input, output) {
      }
      else if(req(input$fb_var)=="fb_p")
      {
-       x <- fb_p <- seq(0.05,fb_p,by=0.05)
+       x <- fb_p <- seq(0.05,fb_p,by=0.005)
        xlab <- "Frequency of disease allele"
      }
      else if(req(input$fb_var)=="fb_alpha")
