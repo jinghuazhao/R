b7728a9da034d3e7046c9e31cfaf2cec076dac44
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-06-14 10:00:58 +0100

    genetic analysis package

--- gap/inst/shinygap/R/global.R
+++ gap/inst/shinygap/R/global.R
@@ -7,6 +7,26 @@ library(vroom)
 library(shiny)
 library(shinydashboard)
 
+KCC <- function(model,GRR,p1,K)
+# 6-6-2018
+{
+   model.idx <- charmatch(model,c("multiplicative","additive","recessive","dominant","overdominant"))
+   if(is.na(model.idx)) stop("Invalid model type")
+   if(model.idx == 0) stop("Ambiguous model type")
+   multiplicative <- c(1,GRR,GRR*GRR)
+   additive <- c(1,GRR,2*GRR-1)
+   recessive <- c(1,1,GRR)
+   dominant <- c(1,GRR,GRR)
+   overdominant <- c(GRR,1,GRR)
+   f <- switch(model.idx,multiplicative,additive,recessive,dominant,overdominant)
+   scale <- K/(f[1]*(1-p1)^2+f[2]*2*p1*(1-p1)+f[3]*p1^2)
+   f <- f*scale
+#  if(f[3]>1) stop("misspecified model")
+   pprime <- (f[3]*p1^2+f[2]*p1*(1-p1))/K
+   p <- ((1-f[3])*p1^2+(1-f[2])*p1*(1-p1))/(1-K)
+   invisible(list(pprime=pprime,p=p))
+}
+
 fbsize <- function (gamma,p,alpha=1e-4,beta=0.2,debug=0,error=0)
 # Family-based sample sizes
 # Jing Hua Zhao 30-12-98, 19-8-2009, 13-6-2021
