d0c7af02e1575dc84c84db3d469d6b5176ed9276
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-07-20 09:52:11 +0100

    genetic analysis package

---
+++ gap/R/runshinygap.R
@@ -0,0 +1,22 @@
+#' Start shinygap
+#'
+#' This function starts the interactive 'shinygap' shiny web application that allows for flexible model specification.
+#'
+#' The 'shiny' based web application allows for flexible model specification for the implemented study designs.
+#'
+#' @param ... Additional arguments passed to the 'runApp' function from the 'shiny' package.
+#' @return These are design specific.
+#'
+
+runshinygap <- function (...)
+{
+    if (requireNamespace("shiny", quietly = TRUE)) {
+        message("Starting the EnsDb shiny web app. Use Ctrl-C to stop.")
+        shiny::runApp(appDir = system.file("shinygap",
+            package = "gap"), ...)
+    }
+    else {
+        stop("Package shiny not installed!")
+    }
+}
+
