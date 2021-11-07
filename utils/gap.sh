3f31ac957bd9956fd0fa438cda86d215d7057fa1
Jing Hua Zhao <jinghuazhao@hotmail.com>
2021-11-07 19:32:51 +0000

    utils

--- utils/gap.sh
+++ utils/gap.sh
@@ -20,3 +20,5 @@ export version=1.2.3-2
 R CMD build --compact-vignettes=both --md5 --resave-data gap
 R CMD INSTALL --compact-docs --data-compress=xz gap_${version}.tar.gz
 R CMD check gap_${version}.tar.gz
+
+rm ${wd}/gap/src/*.so ${wd}/gap/src/*.o
