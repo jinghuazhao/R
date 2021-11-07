377eaa003810884998987aee7aea0bb56d386e76
Jing Hua Zhao <jinghuazhao@hotmail.com>
2021-11-07 19:44:29 +0000

    genetic analysis package

--- gap/R/get_pve_se.R
+++ gap/R/get_pve_se.R
@@ -1,15 +1,15 @@
 #' Get pve and its standard error from n, z
 #'
 #' This function obtains proportion of explained variance of a continuous outcome.
-#' The standard error is based on \eqn{r^2} and appears \eqn{\sqrt{2}} less than that
-#' from variance of the ratio.
+#' The standard error is based on \eqn{r^2} and appears \eqn{\sqrt{2}}-fold less
+#' than that from variance of the ratio.
 #'
 #' @md
 #' @param n Sample size.
 #' @param z z-statistic, i.e., b/se when they are available instead.
 #' @param correction if TRUE an correction based on t-statistic is applied.
 #' @export
-#' @return pve.
+#' @return pve and its se.
 
 get_pve_se <- function(n,z,correction=TRUE)
 {
