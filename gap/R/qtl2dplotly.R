ddb8f983d8f1011afd84520bcca5478a13768703
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-06-08 11:54:11 +0100

    genetic analysis package

--- gap/R/qtl2dplotly.R
+++ gap/R/qtl2dplotly.R
@@ -1,7 +1,7 @@
-#' 2D pQTL plotly
+#' 2D QTL plotly
 #'
 #' @md
-#' @param d Data in pqtl2dplot() format.
+#' @param d Data in qtl2dplot() format.
 #' @param chrlen Lengths of chromosomes for specific build: hg18, hg19, hg38.
 #' @export
 #' @return A plotly figure.
@@ -10,15 +10,15 @@
 #' \dontrun{
 #' INF <- Sys.getenv("INF")
 #' d <- read.csv(file.path(INF,"work","INF1.merge.cis.vs.trans"),as.is=TRUE)
-#' r <- pqtl2dplotly(d)
-#' htmlwidgets::saveWidget(r,file=file.path(INF,"INF1.pqtl2dplotly.html"))
+#' r <- qtl2dplotly(d)
+#' htmlwidgets::saveWidget(r,file=file.path(INF,"INF1.qtl2dplotly.html"))
 #' r
 #' }
 
-pqtl2dplotly <- function(d, chrlen=gap::hg19)
+qtl2dplotly <- function(d, chrlen=gap::hg19)
 {
-  n <- CM <- snpid <- pos_pqtl <- pos_prot <- prot_gene <- lp <- chr1 <- pos1 <- chr2 <- pos2 <- target <- gene <- log10p <- cistrans <- y <- NA
-  t2d <- pqtl2dplot(d, chrlen, plot=FALSE)
+  n <- CM <- snpid <- pos_qtl <- pos_prot <- prot_gene <- lp <- chr1 <- pos1 <- chr2 <- pos2 <- target <- gene <- log10p <- cistrans <- y <- NA
+  t2d <- qtl2dplot(d, chrlen, plot=FALSE)
   n <- with(t2d, n)
   CM <- with(t2d, CM)
   tkvals <- tktxts <- vector()
@@ -27,11 +27,11 @@ pqtl2dplotly <- function(d, chrlen=gap::hg19)
        tktxts[x] <- xy(x)
   }
   t2d_pos <- with(t2d, data) %>%
-             dplyr::mutate(snpid=paste("SNPid:",id),pos_pqtl=paste0("pQTL: ",chr1,":",pos1),
+             dplyr::mutate(snpid=paste("SNPid:",id),pos_qtl=paste0("QTL: ",chr1,":",pos1),
                            pos_prot=paste0("Protein: ",chr2,":",pos2),
                            prot_gene=paste0("target (gene):", target, "(", gene, ")"),
                            lp=paste("-log10(P):", -log10p),
-                           text=paste(snpid, pos_pqtl, pos_prot, prot_gene, lp, sep="\n")) %>%
+                           text=paste(snpid, pos_qtl, pos_prot, prot_gene, lp, sep="\n")) %>%
              dplyr::select(x,y,cistrans,text)
   axes <- list(tickmode = "array",
                tick0 = 1,
@@ -41,7 +41,7 @@ pqtl2dplotly <- function(d, chrlen=gap::hg19)
                tickfont = list(family = "arial", size = 12, color = "#7f7f7f"),
                tickvals = tkvals,
                ticktext = tktxts)
-  xaxis = c(title = "pQTL position", axes)
+  xaxis = c(title = "QTL position", axes)
   yaxis = c(title = "Gene position", axes)
   cols <- c('#BF382A','#0C4B8E')
 # cols <- setNames(cols, c("cis","trans"))
--- gap/R/qtl3dplotly.R
+++ gap/R/qtl3dplotly.R
@@ -1,7 +1,7 @@
-#' 3D pQTL plot
+#' 3D QTL plot
 #'
 #' @md
-#' @param d Data in pqtl2d() format.
+#' @param d Data in qtl2d() format.
 #' @param chrlen Lengths of chromosomes for specific build: hg18, hg19, hg38.
 #' @param zmax Maximum -log10p to truncate, above which they would be set to this value.
 #' @export
@@ -11,15 +11,15 @@
 #' \dontrun{
 #' INF <- Sys.getenv("INF")
 #' d <- read.csv(file.path(INF,"work","INF1.merge.cis.vs.trans"),as.is=TRUE)
-#' r <- pqtl3dplotly(d,zmax=300)
-#' htmlwidgets::saveWidget(r,file=file.path(INF,"INF1.pqtl3dplotly.html"))
+#' r <- qtl3dplotly(d,zmax=300)
+#' htmlwidgets::saveWidget(r,file=file.path(INF,"INF1.qtl3dplotly.html"))
 #' r
 #' }
 
-pqtl3dplotly <- function(d, chrlen=gap::hg19, zmax=300)
+qtl3dplotly <- function(d, chrlen=gap::hg19, zmax=300)
 {
-  n <- CM <- snpid <- pos_pqtl <- pos_prot <- prot_gene <- lp <- chr1 <- pos1 <- chr2 <- pos2 <- target <- gene <- log10p <- cistrans <- y <- NA
-  t2d <- pqtl2dplot(d, chrlen, plot=FALSE)
+  n <- CM <- snpid <- pos_qtl <- pos_prot <- prot_gene <- lp <- chr1 <- pos1 <- chr2 <- pos2 <- target <- gene <- log10p <- cistrans <- y <- NA
+  t2d <- qtl2dplot(d, chrlen, plot=FALSE)
   n <- with(t2d, n)
   CM <- with(t2d, CM)
   tkvals <- tktxts <- vector()
@@ -28,17 +28,17 @@ pqtl3dplotly <- function(d, chrlen=gap::hg19, zmax=300)
        tktxts[x] <- xy(x)
   }
   t2d_pos <- with(t2d, data) %>%
-             dplyr::mutate(snpid=paste("SNPid:",id),pos_pqtl=paste0("pQTL: ",chr1,":",pos1),
+             dplyr::mutate(snpid=paste("SNPid:",id),pos_qtl=paste0("QTL: ",chr1,":",pos1),
                            pos_prot=paste0("Protein: ",chr2,":",pos2),
                            prot_gene=paste0("target (gene):", target, "(", gene, ")"),
                            lp=paste("-log10(P):", -log10p),
-                           text=paste(snpid, pos_pqtl, pos_prot, prot_gene, lp, sep="\n")) %>%
+                           text=paste(snpid, pos_qtl, pos_prot, prot_gene, lp, sep="\n")) %>%
              dplyr::mutate(z=if_else(-log10p<=zmax,-log10p,zmax)) %>%
              dplyr::select(x,y,z,cistrans,text)
   fig <- with(t2d_pos,
          plotly::plot_ly(t2d_pos, x = ~x, y = ~y, z = ~z, color = ~cistrans, colors = c('#BF382A', '#0C4B8E')) %>%
          plotly::add_markers(type="scatter3d", text=text) %>%
-         plotly::layout(scene = list(xaxis = list(title = "pQTL position",
+         plotly::layout(scene = list(xaxis = list(title = "QTL position",
                                                   tickmode = "array",
                                                   autotick = FALSE,
                                                   tick0 = 1,
