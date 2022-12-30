a88c0053db7ff5f2d2cfb0d9d85a23f3688c0c67
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-12-30 13:27:34 +0000

    utils

--- utils/gap.sh
+++ utils/gap.sh
@@ -8,21 +8,17 @@ export wd=~/R
 cd ${wd}/gap
 Rscript -e "devtools::document()"
 cd ${wd}/gap/inst/shinygap/
-
-function rmd()
-{
-  Rscript -e "knitr::knit('README.Rmd')"
-  pandoc README.md --citeproc --mathjax -s --self-contained -o index.html
-}
-
 cp README.Rmd ${wd}/gap/vignettes/shinygap.Rmd
 cp shinygap.bib ${wd}/gap/vignettes/shinygap.bib
-cd ${wd}
 
+# Rscript -e "knitr::knit('README.Rmd')"
+# pandoc README.md --citeproc --mathjax -s --self-contained -o index.html
+
+cd ${wd}
 export version=1.3-2
 
 R CMD build --compact-vignettes=both --md5 --resave-data --log gap
 R CMD INSTALL --compact-docs --data-compress=xz gap_${version}.tar.gz
-R CMD check gap_${version}.tar.gz
+R CMD check --as-cran gap_${version}.tar.gz
 
 rm ${wd}/gap/src/*.so ${wd}/gap/src/*.o
