4a4e4022e726df59522416fef5be41f312991cc0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-01-05 10:13:19 +0000

    utils

--- utils/gap.sh
+++ utils/gap.sh
@@ -7,6 +7,6 @@ Rscript -e 'setwd("gap");devtools::document()'
 
 export version=1.3-2
 R CMD build --compact-vignettes=both --md5 --resave-data --log gap
+rm gap/src/*.so gap/src/*.o
 R CMD INSTALL --compact-docs --data-compress=xz gap_${version}.tar.gz
 R CMD check --as-cran gap_${version}.tar.gz
-rm gap/src/*.so gap/src/*.o
