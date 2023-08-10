99a382480c1cd35cd4c547b27161db82781310c7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-08-10 11:21:44 +0100

    utils

--- utils/suggests.R
+++
@@ -1,9 +0,0 @@
-# gap
-install.packages(c("BradleyTerry2","MASS","MCMCglmm","R2jags","Rdpack","bdsmatrix","bookdown",
-                   "calibrate", "circlize", "coda", "coxme", "cowplot", "coxme", "dplyr", "foreign", "forestplot",
-                   "gap.datasets", "genetics", "ggplot2", "grid", "haplo.stats", "jsonlite", "kinship2",
-                   "lattice", "magic", "matrixStats", "meta", "metafor", "mets", "nlme", "pedigree",
-                   "pedigreemm", "plotly", "plotrix", "regress", "reshape", "rmeta", "rms", "survival"),
-                   repos="https://cran.r-project.org")
-# pan
-install.packages("mitools")
