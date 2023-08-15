06e9ca81e80bfef23dfed226b1e86011a9223d77
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-08-15 14:25:12 +0100

    genetic analysis package

--- gap/R/METAL_forestplot.R
+++ gap/R/METAL_forestplot.R
@@ -3,9 +3,8 @@
 #' @param tbl Meta-anslysis summary statistics.
 #' @param all statistics from all contributing studies.
 #' @param rsid SNPID-rsid mapping file.
-#' @param package style of plot as in meta, rmeta or forestplot.
+#' @param random a flag for fixed/random effects model.
 #' @param split when TRUE, individual prot-MarkerName.pdf will be generated.
-#' @param random a flag for enable/disable random effect model.
 #' @param ... Additional arguments to meta::forest.
 #'
 #' @details
@@ -42,18 +41,14 @@
 #' @author Jing Hua Zhao
 #' @keywords hplot distribution
 
-METAL_forestplot <- function(tbl,all,rsid,package="meta",random=TRUE,split=FALSE,...)
+METAL_forestplot <- function(tbl,all,rsid,random=TRUE,split=FALSE,...)
 {
   prot <- MarkerName <- NA
   requireNamespace("dplyr")
   dplyr_rsid <- function(df,rsid)
   {
-  # d <- dplyr::nest_join(df,rsid)
     d <- dplyr::left_join(df,rsid)
-  # dy <- d["y"]
     m <- within(d, {
-  #   rsid <- ifelse(length(lapply(dy,"[[",1)) == 1, unlist(d[["y"]]), unlist(lapply(lapply(dy,"[[",1),"[",1)))
-  #   rsid <- unlist(d[["y"]])
       isna <- is.na(rsid)
       rsid[isna] <- MarkerName[isna]
     })
@@ -85,39 +80,11 @@ METAL_forestplot <- function(tbl,all,rsid,package="meta",random=TRUE,split=FALSE
        if (package=="meta")
        {
          requireNamespace("meta")
-         mg <- meta::metagen(BETA,SE,sprintf("%s (%.0f)",study,N),title=title,method.tau.ci="",random=random)
+         mg <- meta::metagen(BETA,SE,sprintf("%s (%.0f)",study,N),title=title,random=random,method.tau.ci="")
          meta::forest(mg,colgap.forest.left = "1cm",leftlabs=c("Study","b","SE"),...)
          requireNamespace("grid")
          grid::grid.text(title,0.5,0.9)
          with(mg,cat("prot =", p, "MarkerName =", m, "Q =", Q, "df =", df.Q, "p =", pval.Q, "I2 =", I2, "lower.I2 =", lower.I2, "upper.I2 =", upper.I2, "\n"))
-       }
-       else if(package=="forestplot")
-       {
-         tabletext <- cbind(c("Study",study,"Summary"),
-                              c("Effect",format(BETA,digits=3),format(tbl[i,"Effect"],digits=3)),
-                              c("SE",format(SE,digits=3),format(tbl[i,"StdErr"],digits=3)),
-                              c("N",N,tbl[i,"N"]))
-         print(tabletext)
-         requireNamespace("forestplot")
-         forestplot::forestplot(tabletext,
-                    c(NA,BETA,tbl[i,"Effect"]),
-                    c(NA,BETA-1.96*SE,tbl[i,"Effect"]-1.96*tbl[i,"StdErr"]),
-                    c(NA,BETA+1.96*SE,tbl[i,"Effect"]+1.96*tbl[i,"StdErr"]),
-                    zero=0,
-                    is.summary=c(TRUE,rep(FALSE,length(BETA)),TRUE),
-                    boxsize=0.75,
-                    col=rmeta::meta.colors(box="royalblue",line="darkblue", summary="royalblue"))
-         title(title)
-       }
-       else if(package=="rmeta")
-       {
-         requireNamespace("rmeta")
-         rmeta::metaplot(BETA,SE,N,
-                  labels=sprintf("%s (%.3f %.3f %.0f)",study,BETA,SE,N),
-                  xlab="Effect distribution",ylab="",xlim=c(-1.5,1.5),
-                  summn=tbl[i,"Effect"],sumse=tbl[i,"StdErr"],sumnn=tbl[i,"N"],
-                  colors=rmeta::meta.colors(box="red",lines="blue", zero="green", summary="red", text="black"))
-         title(title)
        }
        if (split) dev.off()
      })
