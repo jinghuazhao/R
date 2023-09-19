8a3d688f9c6baa519e8dc1b2ab5af06773e65d64
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-09-19 20:28:28 +0100

    genetic analysis package

--- gap/R/find_qtls.R
+++ gap/R/find_qtls.R
@@ -12,10 +12,15 @@
 #' @param log10P -log(P).
 #' @param N sample size.
 #' @param radius a flanking distance.
+#' @param collapse.hla a flag to collapse signals in the HLA region.
+#' @param build genome build to define the HLA region.
 #'
 #' @details
 #' This function implements an iterative merging algorithm to identify signals.
-#' The setup follows output from METAL.
+#' The setup follows output from METAL. When collapse.hla=TRUE, a single most
+#' significant signal in the HLA region is chosen. The Immunogenomics paper gives
+#' GRCh37: chr6:28477797-33448354 (6p22.1-21.3),
+#' GRCh38: chr6:28510020-33480577.
 #'
 #' @export
 #' @return The function lists QTLs and meta-information.
@@ -34,7 +39,7 @@ find_qtls <- function(d, Chromosome="Chromosome",Position="Position",
                          MarkerName="MarkerName",Allele1="Allele1",Allele2="Allele2",
                          EAF="Freq1",
                          Effect="Effect",StdErr="StdErr",log10P="log10P",
-                         N="N", radius=1e6)
+                         N="N",radius=1e6,collapse.hla=TRUE,build="hg19")
 {
   for (q in c("dplyr","valr")) {
      if (length(grep(paste("^package:", q, "$", sep=""), search())) == 0) {
@@ -42,14 +47,34 @@ find_qtls <- function(d, Chromosome="Chromosome",Position="Position",
         warning(paste("find_qtls needs package `", q, "' to be fully functional; please install", sep=""))
      }
   }
-  chrom <- start.gene <- end.gene <- geneStart <- geneEnd <- mlog10p <- NULL
+  chrom <- start.gene <- end.gene <- geneStart <- geneEnd <- mlog10p <- hla_qtls <- other_qtls <- pQTLs <- NULL
   p <- data.frame(chrom=paste0("chr",d[[Chromosome]]),start=d[[Position]],end=d[[Position]],
                   rsid=d[[MarkerName]],a1=d[[Allele1]],a2=d[[Allele2]],
                   EAF=d[[EAF]],b=d[[Effect]],SE=d[[StdErr]],mlog10p=-d[[log10P]],n=d[[N]])
   m <- valr::bed_merge(p,max_dist=radius)
   i <- valr::bed_intersect(p,m,suffix = c("", ".gene")) %>%
        dplyr::rename(geneStart=start.gene,geneEnd=end.gene)
-  pQTLs <- i %>%
-           dplyr::group_by(chrom,geneStart,geneEnd) %>%
-           dplyr::slice_max(mlog10p)
+  if (nrow(i)>0)
+  {
+    if (!collapse.hla)
+       pQTLs <- i %>%
+                dplyr::group_by(chrom,geneStart,geneEnd) %>%
+                dplyr::slice_max(mlog10p)
+    else
+    {
+      if (build=="hg19") {startHLA <- 28477797; endHLA <- 33448354} else {startHLA <- 28510020; endHLA <- 33480577}
+      hla <- with(i, chrom=="chr6" & start >= startHLA & end <= endHLA)
+      hasHLA <- dplyr::filter(i,hla)
+      hasOther <- dplyr::filter(i,!hla)
+      if (nrow(hasHLA)>0) hla_qtls <- hasHLA %>%
+                                      dplyr::group_by(chrom,geneStart,geneEnd) %>%
+                                      dplyr::slice_max(mlog10p)
+      if (nrow(hasOther)>0) other_qtls <- hasOther %>%
+                                          dplyr::group_by(chrom,geneStart,geneEnd) %>%
+                                          dplyr::slice_max(mlog10p) %>%
+                                          bind_rows(hla_qtls)
+      pQTLs <- bind_rows(hla_qtls,other_qtls)
+    }
+  }
+  pQTLs
 }
