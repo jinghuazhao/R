597af1fe20fe21aa978ea57923ee0c32fc8e45b7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-01-03 18:48:33 +0000

    docs

--- docs/csd3-gcc11.sh
+++ docs/csd3-gcc11.sh
@@ -4,17 +4,10 @@ module load gcc/11 geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv
 module load image-magick-7.0.5-9-gcc-5.4.0-d4lemcc
 module load ceuadmin/glpk/4.57 ceuadmin/icu/70.1
 
-export prefix=/rds-d4/user/$USER/hpc-work
-export prefix=/rds/project/jmmh2/rds-jmmh2-public_databases/software
-cd ${prefix}
 export version=4.3.3
-cd R-${version}-gcc11
-export gcc11=/usr/local/software/master/gcc/11
-export intl=/usr/local/software/spack/spack-0.11.2/opt/spack/linux-rhel7-x86_64/gcc-5.4.0/gettext-0.19.8.1-5iqkv5zrractwd57vydu5czosgtrlwj2/
-export HPC_WORK=/rds/user/jhz22/hpc-work
-export include=${gcc11}/include:${intl}/include:${HPC_WORK}/include
-export ldflags=${gcc11}/lib64:${gcc11}/lib:${intl}/lib:${HPC_WORK}/lib64:${HPC_WORK}/lib
+export prefix=/rds/project/jmmh2/rds-jmmh2-public_databases/software/R-${version}-gcc11
+cd ${prefix}
 ./configure --prefix=${prefix} \
             --with-pcre2 \
-            --enable-R-shlib CPPFLAGS=-I${include} LDFLAGS=-L${ldflags} LIBS=-ltinfo LIBS=-lintl
+            --enable-R-shlib
 make
--- docs/csd3-icelake.sh
+++ docs/csd3-icelake.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-module load gcc/8 geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv5z pcre2-10.20-gcc-5.4.0-tcuhtrb texlive
-module load curl/7.83.0/gcc/ozlrq5hx
+module load gcc/8 geos-3.6.2-gcc-5.4.0-vejexvy libiconv/1.16/gcc/4miyzf3w pcre2/10.36/gcc/sya23vzi texlive
+module load curl/7.83.0/gcc/ozlrq5hx gettext/0.21/gcc/qnrcglqo
 module load image-magick-7.0.5-9-gcc-5.4.0-d4lemcc readline/8.1/gcc/bgw44yb2
+module load ceuadmin/json-c/0.17-20230812-icelake
 module load ceuadmin/glpk/4.57 ceuadmin/icu/70.1 ceuadmin/nettle/2.7.1 ceuadmin/openssl/3.2.1-icelake
 
 export version=4.3.3
@@ -11,14 +12,8 @@ export prefix=/rds/project/jmmh2/rds-jmmh2-public_databases/software/R-${version
 mkdir -p ${prefix} && cd ${prefix}
 wget -qO- https://cran.r-project.org/src/base/R-${major}/R-${version}.tar.gz | \
 tar xvfz - -C ${prefix}
-mv */* .
-rmdir R-${version}
-export gcc8=/usr/local/software/master/gcc/8
-export intl=/usr/local/software/spack/spack-0.11.2/opt/spack/linux-rhel7-x86_64/gcc-5.4.0/gettext-0.19.8.1-5iqkv5zrractwd57vydu5czosgtrlwj2/
-export HPC_WORK=/rds/user/jhz22/hpc-work
-export include=${gcc8}/include:${intl}/include:${HPC_WORK}/include
-export ldflags=${gcc8}/lib64:${gcc8}/lib:${intl}/lib:${HPC_WORK}/lib64:${HPC_WORK}/lib
-./configure --prefix=${prefix}/R-${version}-icelake \
+cd ${prefix}
+./configure --prefix=${prefix} \
             --with-pcre2 \
-            --enable-R-shlib CPPFLAGS=-I${include} LDFLAGS=-L${ldflags} LIBS=-ltinfo LIBS=-lintl
+            --enable-R-shlib
 make
