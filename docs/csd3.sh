30c11c167bc0df05c9f24f09c0efcdffca45f4b7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-03-01 22:27:18 +0000

    docs

--- docs/csd3.sh
+++ docs/csd3.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 module load gcc/6 geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv5z pcre2-10.20-gcc-5.4.0-tcuhtrb texlive
+module load image-magick-7.0.5-9-gcc-5.4.0-d4lemcc
 export prefix=/rds-d4/user/$USER/hpc-work
-export prefix=~/rds/public_databases/software
+export prefix=/rds/project/jmmh2/rds-jmmh2-public_databases/software
 cd ${prefix}
-export version=4.3.2
+export version=4.3.3
 IFS=\. read major minor1 minor2 <<<${version}
 wget -qO- https://cran.r-project.org/src/base/R-${major}/R-${version}.tar.gz | \
 tar xvfz -
