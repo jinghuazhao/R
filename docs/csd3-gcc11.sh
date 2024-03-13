83aa45695013a8da35401bc5440fc8f5468488cb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-03-13 22:40:29 +0000

    docs

--- docs/csd3-gcc11.sh
+++ docs/csd3-gcc11.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-module load gcc/6 geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv5z pcre2-10.20-gcc-5.4.0-tcuhtrb texlive
+module load gcc/11 geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv5z pcre2-10.20-gcc-5.4.0-tcuhtrb texlive
 module load image-magick-7.0.5-9-gcc-5.4.0-d4lemcc
 module load ceuadmin/glpk/4.57 ceuadmin/icu/70.1
 
@@ -8,10 +8,7 @@ export prefix=/rds-d4/user/$USER/hpc-work
 export prefix=/rds/project/jmmh2/rds-jmmh2-public_databases/software
 cd ${prefix}
 export version=4.3.3
-IFS=\. read major minor1 minor2 <<<${version}
-wget -qO- https://cran.r-project.org/src/base/R-${major}/R-${version}.tar.gz | \
-tar xvfz -
-cd R-${version}
+cd R-${version}-gcc11
 export gcc6=/usr/local/software/master/gcc/6
 export intl=/usr/local/software/spack/spack-0.11.2/opt/spack/linux-rhel7-x86_64/gcc-5.4.0/gettext-0.19.8.1-5iqkv5zrractwd57vydu5czosgtrlwj2/
 export HPC_WORK=/rds/user/jhz22/hpc-work
@@ -21,6 +18,3 @@ export ldflags=${gcc6}/lib64:${gcc6}/lib:${intl}/lib:${HPC_WORK}/lib64:${HPC_WOR
             --with-pcre2 \
             --enable-R-shlib CPPFLAGS=-I${include} LDFLAGS=-L${ldflags} LIBS=-ltinfo LIBS=-lintl
 make
-make install
-ln -sf  ${prefix}/R-${version}/bin/R $HOME/bin/R
-Rscript -e 'update.packages(checkBuilt=TRUE,ask=FALSE)'
