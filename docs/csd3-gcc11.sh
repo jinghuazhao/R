d28c5262c8d35e40bb1df3eba9146be13d57715f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-03-22 10:44:12 +0000

    docs

--- docs/csd3-gcc11.sh
+++ docs/csd3-gcc11.sh
@@ -9,11 +9,11 @@ export prefix=/rds/project/jmmh2/rds-jmmh2-public_databases/software
 cd ${prefix}
 export version=4.3.3
 cd R-${version}-gcc11
-export gcc6=/usr/local/software/master/gcc/6
+export gcc11=/usr/local/software/master/gcc/11
 export intl=/usr/local/software/spack/spack-0.11.2/opt/spack/linux-rhel7-x86_64/gcc-5.4.0/gettext-0.19.8.1-5iqkv5zrractwd57vydu5czosgtrlwj2/
 export HPC_WORK=/rds/user/jhz22/hpc-work
-export include=${gcc6}/include:${intl}/include:${HPC_WORK}/include
-export ldflags=${gcc6}/lib64:${gcc6}/lib:${intl}/lib:${HPC_WORK}/lib64:${HPC_WORK}/lib
+export include=${gcc11}/include:${intl}/include:${HPC_WORK}/include
+export ldflags=${gcc11}/lib64:${gcc11}/lib:${intl}/lib:${HPC_WORK}/lib64:${HPC_WORK}/lib
 ./configure --prefix=${prefix} \
             --with-pcre2 \
             --enable-R-shlib CPPFLAGS=-I${include} LDFLAGS=-L${ldflags} LIBS=-ltinfo LIBS=-lintl
