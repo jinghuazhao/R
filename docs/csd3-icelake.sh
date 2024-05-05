b42b434e3f149fe0bee7ec66eef9a1ba51c8474c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-05-05 16:04:40 +0100

    docs

--- docs/csd3-icelake.sh
+++ docs/csd3-icelake.sh
@@ -19,6 +19,7 @@ tar xfz - --no-same-owner -C ${dest} --strip-components=1
 cd ${dest}
 ./configure --prefix=${prefix}/${dest} --with-pcre2 --enable-R-shlib
 make
+make install
 
 function legacy()
 {
