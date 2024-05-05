953c11d6d5c13d8944fa293dc40bccf24f4ee45e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-05-05 21:20:51 +0100

    docs

--- docs/csd3.sh
+++ docs/csd3.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
 
-module load gcc/9 geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv5z pcre2-10.20-gcc-5.4.0-tcuhtrb texlive
+module load gcc/6 geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv5z pcre2-10.20-gcc-5.4.0-tcuhtrb texlive
 module load image-magick-7.0.5-9-gcc-5.4.0-d4lemcc
 module load ceuadmin/brotli/1.0.9 ceuadmin/glpk/4.57 ceuadmin/icu/70.1 ceuadmin/jq/1.6 protobuf-3.4.0-gcc-5.4.0-zkpendv
 module load ceuadmin/NLopt/2.7.1
-
-module unload gcc/6
 
 export version=4.4.0
 IFS=\. read major minor1 minor2 <<<${version}
