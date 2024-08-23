9e50e02aab4f53d511533d31d02345b2425e635b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-08-23 13:37:58 +0100

    docs

--- docs/csd3.sh
+++ docs/csd3.sh
@@ -1,39 +1,35 @@
 #!/usr/bin/bash
 
-module load gcc/6
-module load curl-7.63.0-gcc-5.4.0-4uswlql geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv5z
-module load image-magick-7.0.5-9-gcc-5.4.0-d4lemcc
-module load mono/5.0.1.1 netcdf/4.4.1
-module load pcre2-10.20-gcc-5.4.0-tcuhtrb protobuf-3.4.0-gcc-5.4.0-zkpendv texlive
-module load ceuadmin/brotli/1.0.9 ceuadmin/icu/70.1 ceuadmin/jq/1.6
-module load ceuadmin/glpk/4.57 ceuadmin/NLopt/2.7.1
-module load ceuadmin/readline/8.0 ceuadmin/rtmpdump/2.3
+module load curl/7.79.0/gcc/75dxv7ac gettext/0.21/gcc/qnrcglqo libiconv/1.16/intel/64iicvbf
+module load libpng/1.6.37/intel/jfrl6z6c pcre2/10.36/gcc/sya23vzi readline/8.1/gcc/bumlt4j6
+module load texlive
+module load libdeflate/1.10/gcc/6ij3yqv2
+module load ceuadmin/json-c/0.17-20230812-icelake ceuadmin/krb5/1.21.2-icelake
+module load ceuadmin/nettle/3.9-icelake
+
+module unload gcc/6
 
 export version=4.4.1
 IFS=\. read major minor1 minor2 <<<${version}
-export rds=/rds/project/jmmh2/rds-jmmh2-public_databases/software
+export rds=/rds/project/rds-4o5vpvAowP0/software
 export prefix=$CEUADMIN/R
-export dest=${version}
-export R_LIBS=${rds}/R
-cd ${prefix}
-mkdir ${dest}
+export dest=${version}-icelake
 umask 022
+cd ${rds}
+mkdir ${dest}
 wget -qO- https://cran.r-project.org/src/base/R-${major}/R-${version}.tar.gz | \
 tar xfz - --no-same-owner -C ${dest} --strip-components=1
 cd ${dest}
 ./configure --prefix=${prefix}/${dest} --with-pcre2 --enable-R-shlib
 make
-bin/Rscript -e 'update.packages(checkBuilt=TRUE,ask=FALSE)'
+make install
+export R_LIBS=${rds}/R
 
-function pre_4.3.3()
+function legacy()
 {
-  export gcc6=/usr/local/software/master/gcc/6
-  export intl=/usr/local/software/spack/spack-0.11.2/opt/spack/linux-rhel7-x86_64/gcc-5.4.0/gettext-0.19.8.1-5iqkv5zrractwd57vydu5czosgtrlwj2/
-  export HPC_WORK=/rds/user/jhz22/hpc-work
-  export include=${gcc6}/include:${intl}/include:${HPC_WORK}/include
-  export ldflags=${gcc6}/lib64:${gcc6}/lib:${intl}/lib:${HPC_WORK}/lib64:${HPC_WORK}/lib
-  cd R-${version}
-  ./configure --prefix=${prefix} \
-              --with-pcre2 \
-              --enable-R-shlib CPPFLAGS=-I${include} LDFLAGS=-L${ldflags} LIBS=-ltinfo LIBS=-lintl
+  module load gcc/8 geos-3.6.2-gcc-5.4.0-vejexvy libiconv/1.16/gcc/4miyzf3w pcre2/10.36/gcc/sya23vzi texlive
+  module load curl/7.83.0/gcc/ozlrq5hx gettext/0.21/gcc/qnrcglqo
+  module load image-magick-7.0.5-9-gcc-5.4.0-d4lemcc libpng/1.6.37/intel/jfrl6z6c readline/8.1/gcc/bgw44yb2
+  module load ceuadmin/json-c/0.17-20230812-icelake
+  module load ceuadmin/glpk/4.57 ceuadmin/icu/70.1 ceuadmin/nettle/2.7.1 ceuadmin/openssl/3.2.1-icelake
 }
--- docs/gap.sh
+++ docs/gap.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash
 
-if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
-   module load ceuadmin/R/4.3.3-icelake
-else
-   module load ceuadmin/R
-fi
+module load ceuadmin/R
 
 cd ~/R
 Rscript -e 'setwd("gap");devtools::document()'
 
 export version=$(awk '/Version/{print $2}' ~/R/gap/DESCRIPTION)
+export R_HOME=/usr/local/Cluster-Apps/ceuadmin/R/4.4.1-icelake/lib64/R
+
 R CMD build --compact-vignettes=both --md5 --resave-data --log gap
 rm gap/src/*.so gap/src/*.o
 R CMD INSTALL --compact-docs --data-compress=xz gap_${version}.tar.gz
