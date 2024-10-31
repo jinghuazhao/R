c749ce7e07d0361239c1ef1f4340006c1df6c0e6
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-10-31 21:54:36 +0000

    docs

--- docs/csd3.sh
+++ docs/csd3.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
-# 23/9/2024 onwards
-
+# 31/10/2024 onwards
 module load hdf5/1.12.1 icu4c/67.1/gcc/maavowaj libpng/1.6.37/intel/jfrl6z6c
-module load mono/5.0.1.1  netcdf/4.4.1 nlopt-2.4.2-gcc-5.4.0-dux2fmz pcre2/10.36/gcc/sya23vzi texlive/2015
+module load mono/5.0.1.1 netcdf/4.4.1 nlopt-2.4.2-gcc-5.4.0-dux2fmz
+module load pcre2/10.36/gcc/sya23vzi texlive/2015
+module load jags-4.3.0-gcc-5.4.0-4z5shby
 module load ceuadmin/libsodium ceuadmin/rust
+module load rstudio/2024.04.2+764
 
 function my_load()
 {
@@ -18,7 +20,7 @@ function my_load()
   module unload gcc/6
 }
 
-export version=4.4.1
+export version=4.4.2
 IFS=\. read major minor1 minor2 <<<${version}
 export rds=/rds/project/rds-4o5vpvAowP0/software
 export prefix=$CEUADMIN/R
