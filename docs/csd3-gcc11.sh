64f257fb6bec68e5ee5580928ff8ed3c681d129f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-01-29 16:52:16 +0000

    docs

--- docs/csd3-gcc11.sh
+++ docs/csd3-gcc11.sh
@@ -2,10 +2,8 @@
 
 module load gcc/11 geos-3.6.2-gcc-5.4.0-vejexvy gettext-0.19.8.1-gcc-5.4.0-5iqkv5z pcre2-10.20-gcc-5.4.0-tcuhtrb texlive
 module load image-magick-7.0.5-9-gcc-5.4.0-d4lemcc
-module load ceuadmin/brotli/1.0.9 ceuadmin/glpk/4.57 ceuadmin/icu/70.1 ceuadmin/jq/1.6 protobuf-3.4.0-gcc-5.4.0-zkpendv
-module load ceuadmin/NLopt/2.7.1 curl/7.79.0/gcc/75dxv7ac
-
-module unload gcc/6
+module load ceuadmin/brotli/1.1.0 ceuadmin/glpk/4.57 ceuadmin/icu/70.1 ceuadmin/jq/1.6 protobuf-3.4.0-gcc-5.4.0-zkpendv
+module load ceuadmin/NLopt/2.7.1 curl/7.79.0/gcc/75dxv7ac ceuadmin/libiconv/1.17
 
 export version=4.4.2
 IFS=\. read major minor1 minor2 <<<${version}
@@ -19,5 +17,7 @@ umask 022
 wget -qO- https://cran.r-project.org/src/base/R-${major}/R-${version}.tar.gz | \
 tar xfz - --no-same-owner -C ${dest} --strip-components=1
 cd ${dest}
-./configure --prefix=${prefix}/${dest} --with-pcre2 --enable-R-shlib
+./configure --prefix=${prefix}/${dest} --with-pcre2 --enable-R-shlib \
+            CPPFLAGS=-I/usr/local/Cluster-Apps/ceuadmin/libiconv/1.17/include \
+            LDFLAGS='-L/usr/local/Cluster-Apps/ceuadmin/libiconv/1.17/lib -liconv'
 make
