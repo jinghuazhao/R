902cfc88e5a7194e0e2dd715dbd066b9117b2fe1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-03-25 11:21:33 +0000

    docs

--- docs/csd3.sh
+++ docs/csd3.sh
@@ -5,7 +5,7 @@ module load hdf5/1.12.1 icu4c/67.1/gcc/maavowaj libpng/1.6.37/intel/jfrl6z6c
 module load mono/5.0.1.1 netcdf/4.4.1
 module load pcre2/10.36/gcc/sya23vzi texlive/2015
 module load jags-4.3.0-gcc-5.4.0-4z5shby
-module load ceuadmin/libsodium ceuadmin/rust ceuadmin/libiconv/1.17
+module load ceuadmin/libsodium ceuadmin/rust ceuadmin/libiconv/1.17 ceuadmin/NLopt/2.7.1
 module load rstudio/2024.04.2+764
 
 function my_load()
