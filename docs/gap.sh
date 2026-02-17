10404ce533f03f15994122136447a180020ae95a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2026-02-17 14:06:53 +0000

    docs

--- docs/gap.sh
+++ docs/gap.sh
@@ -10,4 +10,5 @@ export version=$(awk '/Version/{print $2}' ~/R/gap/DESCRIPTION)
 R CMD build --compact-vignettes=both --md5 --resave-data --log gap
 rm gap/src/*.so gap/src/*.o
 R CMD INSTALL --compact-docs --data-compress=xz gap_${version}.tar.gz
+R CMD check --use-valgrind gap_${version}.tar.gz
 R CMD check --as-cran gap_${version}.tar.gz
