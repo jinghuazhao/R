5b08cb0ca530c9b3e697e8f62de2ac4e78b1510f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2026-03-31 08:15:20 +0100

    docs

--- docs/gap.sh
+++ docs/gap.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/bash
 
-module load ceuadmin/R
-
 cd ~/R
+module load ceuadmin/R
 Rscript -e 'setwd("gap");devtools::document()'
-
 export version=$(awk '/Version/{print $2}' ~/R/gap/DESCRIPTION)
-
 R CMD build --compact-vignettes=both --md5 --resave-data --log gap
 rm gap/src/*.so gap/src/*.o
 R CMD INSTALL --compact-docs --data-compress=xz gap_${version}.tar.gz
