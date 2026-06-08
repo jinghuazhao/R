1b53f33e2ce19f0e20ec656d06f09fa71062e4b1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2026-06-08 16:45:13 +0100

    genetic analysis package

--- gap/R/masize.R
+++ gap/R/masize.R
@@ -55,13 +55,13 @@
 #' coefficient in the full model. Mediation can be expressed as attenuation b1* - b1 or proportion
 #' of total effect explained (PTE):
 #'
-#' \deqn{PTE = \frac{(b_1^*-b_1)}{b_1^*}}
+#' \deqn{PTE = \frac{b_1^*-b_1}{b_1^*}}
 #'
 #' Testing mediation is equivalent (linear models) or approximately equivalent (GLMs and Cox models)
 #' to testing the coefficient of X2 in the full model. Sample size is therefore derived from a Wald
 #' test on b2 using the expected information matrix.
 #'
-#' For binary variables, use \eqn{\sqrt(f(1-f))} for the corresponding standard deviation.
+#' For binary variables, use \eqn{\sqrt{f(1-f)}} for the corresponding standard deviation.
 #'
 #' Linear models parameters are based on \insertCite{hsieh98;textual}{gap} and use a variance inflation
 #' factor \eqn{1/(1-\rho^2)}, where \eqn{\rho} is the correlation between X1 and X2. Logistic and Cox models
@@ -69,14 +69,20 @@
 #' most cases.
 #'
 #' The naming convention is:
-#' "cc" continuous-continuous, "bc" binary-continuous, "cb" continuous-binary, "bb" binary-binary.
-#' Methods ending in "approx" use analytic approximations, while methods ending in "s" use simulation.
+#' - "cc" continuous-continuous.
+#' - "bc" binary-continuous.
+#' - "cb" continuous-binary.
+#' - "bb" binary-binary.
+#' - ".approx" use analytic approximations.
+#' - ".s" use simulation.
 #'
 #' The quantity PTE and b1star parameter for linear model are provided as follows,
 #'
-#' \code{getPTE <- function(b1, b2, rho, sdx1=1, sdx2=1)
-#'                          b2*sdx2*rho/((b1+b2*sdx2*rho/sdx1)*sdx1)}
-#' \code{getb1star <- function(b1, b2, rho, sdx1=1, sdx2=1) b1+b2*sdx2*rho/sdx1}
+#' \preformatted{
+#'  getPTE <- function(b1, b2, rho, sdx1=1, sdx2=1)
+#'                     b2*sdx2*rho/((b1+b2*sdx2*rho/sdx1)*sdx1)
+#'  getb1star <- function(b1, b2, rho, sdx1=1, sdx2=1) b1+b2*sdx2*rho/sdx1
+#'  }
 #'
 #' This also turns out to be the analytic solution in cases CpCm and BpCm for the Poisson model, and underlies approximate
 #' solutions for the logistic and Cox models. An analytic solution is also given for cases CpBm and BpBm for the Poisson
