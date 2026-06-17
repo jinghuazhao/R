c8f1aa1ab5620587e08a224da762a3910c5d34cb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2026-06-17 16:21:04 +0100

    docs

--- docs/gaawr2.sh
+++ docs/gaawr2.sh
@@ -32,13 +32,17 @@ export _R_CHECK_LIMIT_CORES_=TRUE
 export MAKEFLAGS="-j$(nproc)"
 
 cd ~/R
-cp ~/gaawr2/vignettes/gaawr2.Rmd vignettes
+cp ~/gaawr2/vignettes/{gaawr2,web}.Rmd vignettes
 Rscript -e 'cat(R.version.string,"\n");devtools::document("gaawr2")'
 R CMD INSTALL gaawr2
 Rscript -e 'rmarkdown::render("vignettes/gaawr2.Rmd",
             output_file="gaawr2.html", output_dir="vignettes", clean=TRUE, quiet=TRUE);
             knitr::purl("vignettes/gaawr2.Rmd", output="vignettes/gaawr2.R", documentation=0)'
+Rscript -e 'rmarkdown::render("vignettes/web.Rmd",
+            output_file="web.html", output_dir="vignettes", clean=TRUE, quiet=TRUE);
+            knitr::purl("vignettes/web.Rmd", output="vignettes/web.R", documentation=0)'
 cp ~/R/vignettes/gaawr2_cran.Rmd $dst/vignettes/gaawr2.Rmd
+cp ~/R/vignettes/web_cran.Rmd $dst/vignettes/web.Rmd
 R CMD build --compact-vignettes=both gaawr2
 find vignettes -maxdepth 1 -type f \( -name '*.png' -o -name '10081*' \) ! -name 'IL-12B_mhtplot.trunc.png' -delete
 find vignettes -type d -name "gaawr2" -exec rm -rf {} +
